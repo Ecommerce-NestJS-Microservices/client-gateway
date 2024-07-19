f5d407ddf33fd85a9666b4afcb705513116a3f7c
mozadev <ceosmore@gmail.com>
2024-07-19 13:18:07 -0500

    chore: Add environment variables for products microservices host and port

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, PRODUCT_SERVICE } from 'src/config';
+import { object } from 'joi';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productMicroserviceHost,
+          port: 3010
+        }
+
+      },
+      
+    ])
+  ]
 })
-export class ProductsModule {}
+export class ProductsModule {
+
+//   constructor() {
+//   console.log({envs})
+// }
+
+
+}

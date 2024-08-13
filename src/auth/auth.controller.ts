93b311c5f944d3c7c8b28b82bfaea95a6193d57c
mozadev <ceosmore@gmail.com>
2024-08-13 13:21:55 -0500

    feat: Add error handling for registration in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { error } from 'console';
 
 @Controller('auth')
 export class AuthController {
@@ -12,12 +14,17 @@ export class AuthController {
   // send is to send messages and emit is to emit events
   @Post('register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
-    return this.client.send('auth.register.user', registerUserDto)
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error)// This error coming of auth-microservice, pipe capture the error propagate from microservice through NATS
+      })
+    )
   }
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.client.send('auth.login.user', loginUserDto)
+
   }
 
   @Get('verify')
